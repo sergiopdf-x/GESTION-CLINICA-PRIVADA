2a9d63b99f115f7dcf84916a16fc2dd748a83101
sergiopdf-x <stoapanta887@gmail.com>
2026-05-25 12:59:03 +0200

    Proyecto inicial estructura creada

---
+++ src/modelo/Empleado.java
@@ -0,0 +1,12 @@
+package modelo;
+public abstract class Empleado {
+    private String cedula;
+    private String nombre;
+    private int edad;
+    private String telefono;
+    private String correo;
+
+    public Empleado(String cedula,String nombre,int edad,String telefono,String correo){
+
+    }
+}
